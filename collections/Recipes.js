9400471034407a01abf815b1a5ff0b9005de3d47
Tiago Gouvêa <tr617080@oi.corp.net>
2017-08-25 18:20:00 -0300

    Defining A Schema In Meteor

---
+++ collections/Recipes.js
@@ -0,0 +1,35 @@
+Recipes = new Meteor.Collection('recipes');
+
+RecipeSchema = new SimpleSchema({
+
+    name: {
+        type: String,
+        label: "Name"
+    },
+    desc: {
+        type: String,
+        label: "Description"
+    },
+    author: {
+        type: String,
+        label: "Author",
+        autoValue: function () {
+            return this.userId
+        }
+    },
+    createdAt: {
+
+        type: Date,
+        label: "Create At",
+        autoValue: function () {
+
+            return new Date()
+
+        }
+
+    }
+
+
+});
+
+Recipes.attachSchema(RecipeSchema);
