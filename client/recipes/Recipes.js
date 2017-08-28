0079858d901941503b393e43438ccfcfd3161a8d
Tiago Gouvêa <tr617080@oi.corp.net>
2017-08-28 14:20:58 -0300

    Template Level Subscriptions

--- client/recipes/Recipes.js
+++ client/recipes/Recipes.js
@@ -1,4 +1,13 @@
-Meteor.subscribe('recipes');
+Template.Recipes.onCreated(function(){
+
+    var self = this;
+    self.autorun(function(){
+
+        self.subscribe('recipes');
+
+    });
+
+});
 
 Template.Recipes.helpers({
 
