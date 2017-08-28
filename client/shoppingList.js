9fde3c2a81de41e4d2d9bf6ae0ab98aa7059ecb2
Tiago Gouvêa <tr617080@oi.corp.net>
2017-08-28 16:16:39 -0300

    Building The Shopping List

---
+++ client/shoppingList.js
@@ -0,0 +1,20 @@
+Template.ShoppingList.onCreated(function () {
+    
+        var self = this;
+        self.autorun(function () {
+    
+            self.subscribe('recipes');
+    
+        });
+    
+    });
+    
+    Template.ShoppingList.helpers({
+    
+        shoppingList: () => {
+    
+            return Recipes.find({inMenu: true});
+    
+        }
+    
+    });
